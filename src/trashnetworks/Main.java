2959305c2b4c6759564e6bffe5b6327e76254181
ggw3vtg <qfreas@ups.com>
2018-11-21 12:47:38 -0500

    final

---
+++ src/trashnetworks/Main.java
@@ -0,0 +1,16 @@
+/**
+ * Quinn Freas
+ * TrashNetworks.com
+ * Project: High Card
+ * @author Quinn Freas
+ */
+package trashnetworks;
+
+import gui.GUI;
+
+public class Main {
+	public static void main(String[] args) {
+		GUI gui = new GUI();
+		gui.launchApp(args);
+	}
+}
