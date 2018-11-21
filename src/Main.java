5e4592d59f8b72e23611e0622f00f1b0046c73bd
qkfreas <qkfreas@gmail.com>
2018-11-21 12:40:20 -0500

    Initial and final upload.

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+/**
+  * Quinn Freas
+  * Final Project: High Card
+  * @author: Quinn Freas
+  */
+
+public class Main {
+	public static void main(String[] args) {
+// creates object
+		GUI gui = new GUI();
+
+// launches application
+		gui.launchApp(args);
+	}
+}
